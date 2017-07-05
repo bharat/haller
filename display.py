54b02356e8800ef8c374580436df4209d335af57
Bharat Mediratta <bharat@altschool.com>
2017-07-04 21:34:41 -0700

    More fun with streaming

--- display.py
+++ display.py
@@ -11,6 +11,26 @@ def streaming_random(a):
     panel_ids = [x['panelId'] for x in a.panel_positions]
     s = a.effect_stream()
 
+    brt_delta = 2
+    brt_max = 80
+    brt_min = 20
+    brt = int((brt_max + brt_min) / 2)
+    while True:
+        a.brightness = brt
+        for p in panel_ids:
+            s.panel_prepare(p, random.randint(0, 255), random.randint(0, 255), random.randint(0, 255),
+                            transition_time=50)
+        s.panel_strobe()
+        # Aurora can only handle 10 updates a second.
+        time.sleep(5)
+        brt = brt + brt_delta
+        if brt >= brt_max or brt <= brt_min:
+            brt_delta = -brt_delta
+
+def streaming_flash(a):
+    panel_ids = [x['panelId'] for x in a.panel_positions]
+    s = a.effect_stream()
+
     brt_delta = 1
     brt_max = 90
     brt_min = 30
@@ -27,27 +47,49 @@ def streaming_random(a):
         if brt > brt_max or brt < brt_min:
             brt_delta = -brt_delta
 
+
 def streaming_wipe(a):
     panel_ids = [x['panelId'] for x in sorted(a.panel_positions, key=lambda k: k['x'])]
     s = a.effect_stream()
-
     a.brightness = 70
+
+    # random
+    c = [
+        (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+        (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+        (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    ]
+
+    # green
+    c = [
+        (  0, 255, 0),
+        (  0, 192, 0),
+        (  0, 128, 0),
+        (  0,  64, 0),
+        (  0,   0, 0)
+    ]
+
+    idx = [0,
+           int(len(panel_ids) / len(c)),
+           int(len(panel_ids) / len(c) * 2),
+           int(len(panel_ids) / len(c) * 3),
+           int(len(panel_ids) / len(c) * 4)
+       ]
     while True:
-        for panel_id in panel_ids:
-            s.panel_set(panel_id,
-                        random.randint(0, 255),
-                        random.randint(0, 255),
-                        random.randint(0, 255))
-            # Aurora can only handle 10 updates a second.
-            time.sleep(0.2)
+        for i in range(len(c)):
+            s.panel_prepare(panel_ids[idx[i]], c[i][0], c[i][1], c[i][2], transition_time=30)
+            idx[i] = (idx[i] + 1) % len(panel_ids)
+        s.panel_strobe()
+        # Aurora can only handle 10 updates a second.
+        time.sleep(.8)
 
 
 def display(a, args):
-    if args.streaming == 'random':
-        streaming_random(a)
-
-    elif args.streaming == 'wipe':
-        streaming_wipe(a)
+    fn = 'streaming_%s' % args.streaming
+    if fn in globals():
+        globals()[fn](a)
+    else:
+        print('No such display effect: %s' % args.streaming)
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
